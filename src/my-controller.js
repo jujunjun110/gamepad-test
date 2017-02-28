79c56eb3fe683a28bbf387b7ef142eb01cd3248a
Jun Ito <jun_ito@voyagegroup.com>
2017-02-28 21:57:10 +0900

    Update: vive-controller

--- src/my-controller.js
+++ src/my-controller.js
@@ -8,14 +8,9 @@ AFRAME.registerComponent('my-controller', {
     multiple: false,
     init: function () {
         const el = this.el
-        el.addEventListener('buttondown', () => {
-            console.log('button down')
+        el.addEventListener('triggerdown', () => {
+            console.log('trigger down')
         })
-    },
-    update: function (oldData) {},
-    remove: function () {},
-    // tick: function (t) { },
-    pause: function () {},
-    play: function () {}
+    }
 })
 
